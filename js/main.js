4d8d641064a4757ce4cd0685ec210bbbec947caf
Entropiks <mikecorseri@gmail.com>
2019-11-07 11:20:14 -0800

    updating to latest version

--- js/main.js
+++ js/main.js
@@ -1,43 +1,37 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'suh dude',
-    pizza: {
-      title: 'pizza',
-      image: "image placeholder",
-      hasIngredients: true,
-      description: 'pizza is literally the best thing in the world',
-      ingredients: ["dough", "cheese", "sauce", "oil"],
-      calories: 'pizza',
-      fats: 'sandwich',
-      sodium: 'fish',
-      similar: [
-        {
-          title: "Pasta",
-          recipeKey: 1001,
-          similarLink: "link goes here"
-        },
-        {
-          title: "Ice Cream",
-          recipeKey: 1041,
-          similarLink: "link goes here"
-        },
-        {
-          title: "Pasta",
-          recipeKey: 1533,
-          similarLink: "link goes here"
-        },
-        {
-          title: "Eggplant",
-          recipeKey: 0092,
-          similarLink: "link goes here"
-        },
-        {
-          title: "Chicken",
-          recipeKey: 1231,
-          similarLink: "link goes here"
-        }
-      ]
-    }
+    recipes: [
+      {
+        title: "Pasta",
+        ingredients: "eggs, flour, butter",
+        recipeKey: 1001,
+        similarLink: "link goes here"
+      },
+      {
+        title: "Ice Cream",
+        ingredients: "eggs, milk, butter",
+        recipeKey: 1041,
+        similarLink: "link goes here"
+      },
+      {
+        title: "Pasta",
+        ingredients: "eggs, flour, butter",
+        recipeKey: 1533,
+        similarLink: "link goes here"
+      },
+      {
+        title: "Eggplant",
+        ingredients: "eggplant",
+        recipeKey: 0092,
+        similarLink: "link goes here"
+      },
+      {
+        title: "Chicken",
+        ingredients: "1 big chickeny boi",
+        recipeKey: 1231,
+        similarLink: "link goes here"
+      }
+    ]
   }
 })
